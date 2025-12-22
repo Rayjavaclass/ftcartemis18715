8ee2f1c585a729a5dc9cf01eb8f8171679c7c751
Rithik Vyas <rithikvyas2@gmail.com>
2025-12-22 12:56:43 -0800

    v2 code

--- Outtake.java
+++ Outtake.java
@@ -1,108 +1,96 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.arcrobotics.ftclib.util.InterpLUT;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple; // Import needed for reverse
-import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.core.commands.Command;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.hardware.powerable.Powerable;
+import dev.nextftc.hardware.powerable.SetPower;
 
-public class Outtake implements Subsystem {
+public class Outtake {
+    public static DcMotorEx BottomouttakeMotor;
+    public final DcMotorEx flyWheelMotor;
 
-    // Hardware definitions
-    public DcMotorEx topMotor;            // Changed name to match Robot.java
-    public DcMotorEx bottomOuttakeMotor;  // Changed name to match Robot.java
+    public InstantCommand start;
+    public InstantCommand stop;
+    public InstantCommand reverse;
 
-    public Servo turretServo;
-    public Servo feedServo;
-    public Servo hood; // Added hood based on your Robot.java usage
+    public InstantCommand autoStart;
 
-    // Turret Tuning
-    private final double TURRET_CENTER_POS = 0.5;
-    private final double TURRET_MAX_DEGREES = 300.0;
+    public static double targetRPM = 5000.0;
+    public static double tolerenceRPM =100.0; //if it is within 100 rpm then be ready
 
-    // Hood constants
-    public static final double MIN = 0.0;
-    public static final double MAX = 1.0;
+    public static boolean isBusy = false;
 
-    // Commands defined as fields (Standard NextFTC practice)
-    public final Command start;
-    public final Command stop;
-    public final Command reverse;
+    public static InterpLUT lut = new InterpLUT();
+// this is for when hood will be added
 
-    public Outtake(HardwareMap hardwareMap) {
-        // Initialize Hardware
-        turretServo = hardwareMap.get(Servo.class, "turret");
-        feedServo = hardwareMap.get(Servo.class, "feeder");
-        hood = hardwareMap.get(Servo.class, "hood"); // Make sure config has "hood"
+    public Outtake (HardwareMap hwMap) {
+        BottomouttakeMotor = hwMap.get(DcMotorEx.class, "outtakeMotor");
+        BottomouttakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BottomouttakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BottomouttakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        topMotor = hardwareMap.get(DcMotorEx.class, "topOuttake"); // Verify config name!
-        bottomOuttakeMotor = hardwareMap.get(DcMotorEx.class, "bottomOuttake"); // Verify config name!
+        flyWheelMotor = hwMap.get(DcMotorEx.class, "flyWheelMotor");
+        flyWheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flyWheelMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flyWheelMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        // Setup motors
-        // Usually one motor needs to be reversed for shooters to spin correctly
-        // topMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        start = new InstantCommand(()-> {
+            BottomouttakeMotor.setPower(1);
+            flyWheelMotor.setPower(-1);
+            isBusy = true;
 
-        // Define Commands
-        start = new InstantCommand(this::shooterStart);
-        stop = new InstantCommand(this::shooterStop);
-        reverse = new InstantCommand(this::shooterReverse);
+        });
+
+        reverse = new InstantCommand(() -> {
+            BottomouttakeMotor.setPower(-1);
+            flyWheelMotor.setPower(1);
+            isBusy = true;
+        });
+
+        stop = new InstantCommand(()-> {
+            BottomouttakeMotor.setPower(0);
+            flyWheelMotor.setPower(0);
+            isBusy = false;
+        });
     }
 
-    @Override
-    public void periodic() {
-        // Required by NextFTC Subsystem
+    public static double getTopRPM() {
+        return BottomouttakeMotor.getVelocity(AngleUnit.DEGREES) / 6.0;
     }
 
-    // =========================================================
-    //  METHODS FOR MOTORS
-    // =========================================================
-
-    public void shooterStart() {
-        // "Both outtake needs to be running"
-        topMotor.setPower(0.6);
-        bottomOuttakeMotor.setPower(0.6);
+    public double getFlywheelRPM() {
+        return flyWheelMotor.getVelocity(AngleUnit.DEGREES) / 6.0;
     }
 
-    public void shooterStop() {
-        topMotor.setPower(0.0);
-        bottomOuttakeMotor.setPower(0.0);
+    public boolean isReadyToShoot() {
+        double currentRPM = getTopRPM();
+        return Math.abs(currentRPM - targetRPM) <+ tolerenceRPM;
+
     }
 
-    public void shooterReverse() {
-        topMotor.setPower(-0.3);
-        bottomOuttakeMotor.setPower(-0.3);
+    public static double getRPMPercentage() {
+        return (getTopRPM() / targetRPM) * 100;
     }
 
-    // =========================================================
-    //  METHODS FOR TURRET / SENSORS
-    // =========================================================
-
-    public double getTopRPM() {
-        // Convert ticks/sec to RPM (approximate, usually ticks * 60 / encoder_resolution)
-        // Just returning velocity for logic checks
-        return topMotor.getVelocity();
+    public void updateTelemetry(Telemetry telemetry) {
+        double currentRPM = getTopRPM();
+        double flyWheelRPM = getFlywheelRPM();
+        boolean ready = isReadyToShoot();
+        telemetry.addData("Outtake RPM", currentRPM);
+        telemetry.addData("Flywheel RPM", flyWheelRPM);
+        telemetry.addData("Flywheel RPM %", getFlywheelRPM());
+        telemetry.addData("Ready to Shoot", ready);
+        telemetry.addData("Outtake RPM %", getRPMPercentage());
     }
 
-    public double getTurretAngle() {
-        double currentPos = turretServo.getPosition();
-        return (currentPos - TURRET_CENTER_POS) * TURRET_MAX_DEGREES;
-    }
 
-    public void setTurretAngle(double degrees) {
-        double clampedDegrees = Math.max(-90, Math.min(90, degrees));
-        double targetPos = TURRET_CENTER_POS + (clampedDegrees / TURRET_MAX_DEGREES);
-        turretServo.setPosition(targetPos);
-    }
-
-    public boolean isFlywheelReady() {
-        // Check if both motors are running fast enough
-        return topMotor.getPower() > 0.5 && bottomOuttakeMotor.getPower() > 0.5;
-    }
-
-    public void feedBall() {
-        feedServo.setPosition(1.0); // Push
-        // Note: You need logic to retract this (0.0) after a split second
-    }
 }
