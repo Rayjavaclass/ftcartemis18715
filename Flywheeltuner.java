f8f5725e26aad397fcb5dd4e312dbac1c6d276c8
Rithik Vyas <rithikvyas2@gmail.com>
2025-12-23 20:14:38 -0800

    Refactor Flywheeltuner for RPM control and telemetry

--- Flywheeltuner.java
+++ Flywheeltuner.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.tutorials;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,74 +12,102 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 public class Flywheeltuner extends OpMode {
     public DcMotorEx flywheelMotor;
 
-    public double highVelocity = 5000;
-    public double lowVelocity = 900;
+    public double highRPM = 6000;
+    public double lowRPM = 900;
 
-    double curTargetVelocity = highVelocity;
+    double curTargetRPM = highRPM;
 
-    double F = 0;
-    double P = 0;
+    double F = 2;
+    double P = 2.5;
 
-    double[] stepSizes = {10.0, 1.0, 0.1, 0.001, 0.0001};
-    int stepIndex= 1;
+    double[] stepSizes = {6000, 5000, 4000, 3000};
+    int stepIndex = 1;
+
+    boolean lastA, lastB, lastDpadLeft, lastDpadRight, lastDpadUp, lastDpadDown;
+
+    final double TICKS_PER_REV = 28;
+    double m_counter = 0;
+
     @Override
     public void init() {
         flywheelMotor = hardwareMap.get(DcMotorEx.class, "flywheelMotor");
         flywheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         flywheelMotor.setDirection(DcMotor.Direction.REVERSE);
+
         PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
         flywheelMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
-        telemetry.addLine("Initialization complete");
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
     }
 
     @Override
     public void loop() {
-        // use this to get gamepad command, set target velocity, update telemetry
 
-        if(gamepad1.aWasPressed()) {
-            if (curTargetVelocity == highVelocity) {
-                curTargetVelocity = lowVelocity;
-            } else { curTargetVelocity = highVelocity; }
+        boolean aPressed = gamepad1.a && !lastA;
+        boolean bPressed = gamepad1.b && !lastB;
+        boolean dpadLeftPressed = gamepad1.dpad_left && !lastDpadLeft;
+        boolean dpadRightPressed = gamepad1.dpad_right && !lastDpadRight;
+        boolean dpadUpPressed = gamepad1.dpad_up && !lastDpadUp;
+        boolean dpadDownPressed = gamepad1.dpad_down && !lastDpadDown;
 
+        telemetry.addData("Counter: ", m_counter);
+        m_counter++;
+
+        if (aPressed) {
+            curTargetRPM = (curTargetRPM == highRPM) ? lowRPM : highRPM;
+            telemetry.addData("aPressed, curTargetRPM: ", curTargetRPM);
         }
 
-        if (gamepad1.bWasPressed()) {
+        if (bPressed) {
             stepIndex = (stepIndex + 1) % stepSizes.length;
-            telemetry.addData("Step Size", stepSizes[stepIndex]);
+            telemetry.addData("bPressed, curTargetRPM: ", curTargetRPM);
         }
 
-        if (gamepad1.dpadLeftWasPressed()) {
-            F += stepSizes[stepIndex];
-            telemetry.addData("F", F);
+        if (dpadLeftPressed)
+        {
+            curTargetRPM -= stepSizes[stepIndex];
+            telemetry.addData("D-Pad Left Pressed, curTargetRPM: ", curTargetRPM);
         }
 
-        if (gamepad1.dpadRightWasPressed()) {
-            F -= stepSizes[stepIndex];
+        if (dpadRightPressed)
+        {
+            curTargetRPM += stepSizes[stepIndex];
+            telemetry.addData("D-Pad Right Pressed, curTargetRPM: ", "%.0f", curTargetRPM);
         }
-        if (gamepad1.dpadUpWasPressed()) {
-            P += stepSizes[stepIndex];
-        }
-        if (gamepad1.dpadDownWasPressed()) {
-            P -= stepSizes[stepIndex];
-        }
+
+        if (curTargetRPM < 0) curTargetRPM = 0;
+
+        if (dpadUpPressed) P += 0.1;
+        if (dpadDownPressed) P -= 0.1;
 
         PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
         flywheelMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
 
-        flywheelMotor.setVelocity(curTargetVelocity);
+        double targetTicksPerSec = (curTargetRPM * TICKS_PER_REV) / 60.0;
+        telemetry.addData("Just before setting the velocity targetTicksPerSec: ", "%.0f", targetTicksPerSec, " curTargetRPM: ", "%.0f", curTargetRPM);
+        flywheelMotor.setVelocity(targetTicksPerSec);
 
-        double currentVelocity = flywheelMotor.getVelocity();
-        telemetry.addData("Current Velocity", currentVelocity);
-        telemetry.addData("Target Velocity", curTargetVelocity);
+        double curVelocity = flywheelMotor.getVelocity();
+        telemetry.addData("Current Velocity (Ticks/Sec)", "%.2f", curVelocity);
+
+        double currentRPM = (curVelocity * 60.0) / TICKS_PER_REV;
+
+        telemetry.addData("Target RPM", "%.0f", curTargetRPM);
+        telemetry.addData("Current RPM", "%.0f", currentRPM);
+        telemetry.addData("Target RPM", "%.0f", curTargetRPM);
+        telemetry.addData("Error RPM", "%.0f", curTargetRPM - currentRPM);
+        telemetry.addLine();
+        telemetry.addData("P", "%.3f", P);
+        telemetry.addData("F", "%.3f", F);
+        telemetry.addData("Step Size (RPM)", "%.0f", stepSizes[stepIndex]);
+
         telemetry.update();
 
-        double error = curTargetVelocity - currentVelocity;
-
-        telemetry.addData("Tuning P", "% 4f (Dpad Up or Down", P);
-        telemetry.addData("Tuning F", "% 4f (Dpad Left or Right", F);
-        telemetry.addData("Tuning Step Size", "%.4f (B Button" ,stepSizes[stepIndex]);
-        telemetry.update();
-
-
+        lastA = gamepad1.a;
+        lastB = gamepad1.b;
+        lastDpadLeft = gamepad1.dpad_left;
+        lastDpadRight = gamepad1.dpad_right;
+        lastDpadUp = gamepad1.dpad_up;
+        lastDpadDown = gamepad1.dpad_down;
     }
 }
